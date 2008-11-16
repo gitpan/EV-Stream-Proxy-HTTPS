57b0c58cd78261892e413f0ea86212eeaf97ebb1
Alex Efros <powerman@cpan.org>
2008-11-16 18:30:34 +0000

    Import of POWERMAN/EV-Stream-Proxy-HTTPS-1.0.0 from CPAN.
    gitpan-cpan-distribution: EV-Stream-Proxy-HTTPS
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         POWERMAN/EV-Stream-Proxy-HTTPS-1.0.0.tar.gz
    gitpan-cpan-author:       POWERMAN
    gitpan-cpan-maturity:     released

---
+++ t/00.load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'EV::Stream::Proxy::HTTPS' );
+}
+
+diag( "Testing EV::Stream::Proxy::HTTPS $EV::Stream::Proxy::HTTPS::VERSION" );
---
+++ t/config.pm
@@ -0,0 +1,6 @@
+use constant CFG_ONLINE => 'y';
+use constant CFG_HOST   => '78.129.196.40';
+use constant CFG_PORT   => '9611';
+use constant CFG_USER   => 'powerman';
+use constant CFG_PASS   => '6Ey5kf33iW';
+1;
